7a687bd2b4a908fd8bc9d84cd33b015bd621b973
baconstarvation <ant.ucd@gmail.com>
2014-02-13 23:57:35 -0800

    mongolab URI added

--- models/order.js
+++ models/order.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-// mongoose.connect ....
+mongoose.connect('mongodb://heroku_app22201226:livkm1ljb359eh8582evn8022i@ds033709.mongolab.com:33709/heroku_app22201226' || 'localhost');
